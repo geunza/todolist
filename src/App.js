63f895c4ca724f911f194bc9b1fbab2a56ce5560
geunyoung-sbs <safa940812@naver.com>
2022-07-05 03:06:34 +0900

    styled-reset 설치

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import TodoHeader from './components/TodoHeader.js'
-import TodoContents from './components/TodoContents.js'
-import TodoFooter from './components/TodoFooter.js'
+import TodoHeader from './components/TodoHeader.js';
+import TodoContents from './components/TodoContents.js';
+import TodoFooter from './components/TodoFooter.js';
+import { Reset } from 'styled-reset';
 import { useEffect, useState } from 'react'
 // import todos from './data/data.json';
 import todos from './data/data.js';
@@ -9,6 +10,7 @@ function App() {
   
   return (
     <div className="App">
+      <Reset></Reset>
       <TodoHeader todos={todos}/>
       <TodoContents todos={todos}/>
       <TodoFooter />
