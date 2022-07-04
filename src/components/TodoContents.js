147a2838bad7474b4840e6eae63025c86d9f6eed
geunyoung-sbs <safa940812@naver.com>
2022-07-05 04:12:08 +0900

    인풋 데이터 입력

--- src/components/TodoContents.js
+++ src/components/TodoContents.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { useEffect, useState } from 'react'
 
 let ContentBox = styled.div`
     padding:20px 0;
@@ -7,9 +8,32 @@ let ContentBox = styled.div`
 `;
 
 function TodoContents(props){
-
+  let [title, setTitle] = useState('');
+  let [content, setContent] = useState('');
     return(
         <ContentBox>
+            <div className="inputArea">
+              <input type="text" name="" id="" placeholder="타이틀" onChange={ (e)=>{
+                setTitle(e.target.value);
+              } }/>
+              <input type="text" name="" id="" placeholder="컨텐츠" onChange={ (e)=>{
+                setContent(e.target.value);
+              } }/>
+              <button onClick={ ()=>{
+                let length = props.todos.length;
+                let t_text = title;
+                let c_text = content;
+                let obj = {
+                  "id" : length,
+                  "title": t_text,
+                  "content": c_text,
+                  "check": false
+                };
+                let copy = [...props.todos];
+                copy.push(obj);
+                props.setTodos(copy)
+              } }>입력하기</button>
+            </div>
             {
                 props.todos.map(function(v, i, arr){
                     return(
