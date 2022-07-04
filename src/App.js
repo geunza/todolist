65e80bb8cfd544e2d4fb8b83efaf30c93509c1cd
geunza <65527146+geunza@users.noreply.github.com>
2022-07-04 18:07:38 +0900

    json에서 js로 변경

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import TodoHeader from './components/TodoHeader.js'
 import TodoContents from './components/TodoContents.js'
 import TodoFooter from './components/TodoFooter.js'
 import { useEffect, useState } from 'react'
-import todos from './data/data.json';
+// import todos from './data/data.json';
+import todos from './data/data.js';
 function App() {
   
   return (
     <div className="App">
-      <TodoHeader todos={todos.todos}/>
-      {/* <TodoContents todos={todos}/> */}
+      <TodoHeader todos={todos}/>
+      <TodoContents todos={todos}/>
       <TodoFooter />
     </div>
   );
