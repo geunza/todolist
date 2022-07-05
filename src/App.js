f7300484e6d78bcd8a05d775152329805fb31bb9
geunyoung-sbs <safa940812@naver.com>
2022-07-06 03:09:11 +0900

    styled-reset 삭제/ fonts.css 임포트

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import './App.css';
 import TodoHeader from './components/TodoHeader.js';
 import TodoContents from './components/TodoContents.js';
 import TodoFooter from './components/TodoFooter.js';
-import { Reset } from 'styled-reset';
 import { useEffect, useState } from 'react'
+import './static/fonts/fonts.css'
 // import todos from './data/data.json';
 import data from './data/data.js';
+
 
 function App() {
   let [todos, setTodos] = useState(data);
   return (
     <div className="App">
-      <Reset></Reset>
       <TodoHeader todos={todos}/>
       <TodoContents todos={todos} setTodos={setTodos}/>
       <TodoFooter />
