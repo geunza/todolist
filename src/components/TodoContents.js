a55adf12b89decc137313fdec9fa2b6340f2bc2e
geunza <65527146+geunza@users.noreply.github.com>
2022-07-26 17:59:44 +0900

    삭제기능 생성(미완)

--- src/components/TodoContents.js
+++ src/components/TodoContents.js
@@ -3,8 +3,11 @@ import { useEffect, useState } from 'react'
 
 let ContentBox = styled.div`
     padding:20px;
-    border:1px solid #000;
+    background: #000;
+    
     border-width:0 1px;
+    flex:1; overflow-y:auto; 
+    color:#fff; 
     .inputArea{
       display:flex; flex-flow:column; margin:auto; position:relative;
       input{
@@ -15,8 +18,9 @@ let ContentBox = styled.div`
       }
     }
     ul{
+      background:#303030; padding:10px 0; border-radius:5px; 
       &:not(:last-child){
-        padding-bottom:15px; margin-bottom:15px; border-bottom:1px solid #e6e6e6; 
+        margin-bottom:15px; 
       }
     }
 `;
@@ -24,8 +28,10 @@ let ContentBox = styled.div`
 function TodoContents(props){
   let [title, setTitle] = useState('');
   let [content, setContent] = useState('');
-  const onReset = () => {
-    let id = props.todos[props.todos.length-1].id + 1;
+  let id = props.todos[props.todos.length-1].id
+  const addOne = () => {
+    console.log(id);
+    id++;
     let t_text = title;
     let c_text = content;
     if(t_text == ''){
@@ -51,19 +57,21 @@ function TodoContents(props){
               <input type="text" name="" id="ipt_cont" placeholder="컨텐츠" onChange={ (e)=>{
                 setContent(e.target.value);
               } }/>
-              <button onClick={ onReset }>Go!</button>
+              <button onClick={ addOne }>Go!</button>
             </div>
             {
                 props.todos.map(function(v, i, arr){
                     return(
-                        <ul key={i} className={`Hi ${v.check ? 'checked' : ''}`} onClick={ ()=>{ 
-                            let copy = [...props.todos];
-                            copy[i].check = !copy[i].check
-                            props.setTodos(copy);
-                          } }>
-                            <li>{v.id}</li>
+                        <ul key={i}>
+                            <li>{i+1}</li>
                             <li>{v.title}</li>
                             <li>{v.content}</li>
+                            <li><button className="remove" onClick={ ()=>{
+                              let copy = [...props.todos];
+                              let idx = i;
+                              copy.splice(i, 1);
+                              props.setTodos(copy);
+                            } }>remove</button></li>
                         </ul>
                     )
                 })
